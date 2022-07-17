86ec4cde54f622e37185d074d134e19ea351265f
BShreevasReddy <108817946+BShreevasReddy@users.noreply.github.com>
2022-07-17 21:28:09 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,3 @@
+# formula=(true positive + true negative)/(true positive + true negative + false positive + false negative)
+#        =((1.5e+02+1.9e+02)/(1.5e+02+1.9e+02+2+2))
+#        =0.99(approx.)
